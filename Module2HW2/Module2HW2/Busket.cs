40753d9b7d1889891bdd0128f0eef931a51541f4
Hurko Volodymyr <92858780+Hurko-Volodymyr@users.noreply.github.com>
2022-09-16 19:12:39 +0300

    Done

---
+++ Module2HW2/Module2HW2/Busket.cs
@@ -0,0 +1,57 @@
+﻿namespace Module2HW2
+{
+    /// <summary>
+    /// Contains Busket.
+    /// </summary>
+    public class Busket
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Busket"/> class.
+        /// Starts the App.
+        /// </summary>
+        public Busket()
+        {
+            this.BuyingList = new List<Buy>();
+        }
+
+        /// <summary>
+        /// Gets or sets contains Buyinglist.
+        /// </summary>
+        public List<Buy> BuyingList { get; set; }
+
+        /// <summary>
+        /// Gets or sets starts the App.
+        /// </summary>
+        public decimal Sum { get; set; }
+
+        /// <summary>
+        /// Starts the App.
+        /// </summary>
+        /// param name = "boughtProduct"
+        public void Add(Product boughtProduct, int numberOfProduct)
+        {
+            if (boughtProduct is null)
+            {
+                throw new ArgumentNullException(nameof(boughtProduct));
+            }
+
+            this.BuyingList.Add(new Buy { BuyingProduct = boughtProduct, Number = numberOfProduct });
+        }
+
+        /// <summary>
+        /// Starts the App.
+        /// </summary>
+        public void DoSum()
+        {
+            this.BuyingList.ForEach(action: buy => this.Sum += buy.BuyingProduct.Price * buy.Number);
+        }
+
+        /// <summary>
+        /// Starts the App.
+        /// </summary>
+        public void DoRead()
+        {
+        this.BuyingList.ForEach(product => Console.WriteLine(value: $"{product.BuyingProduct.Name} (Цена за штуку: {product.BuyingProduct.Price} грн.) в количестве: {product.Number}"));
+        }
+    }
+}
---
+++ Module2HW2/Module2HW2/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2HW2
+{
+    /// <summary>
+    /// Contains Product.
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Gets or sets main Program.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets main Program.
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+}
